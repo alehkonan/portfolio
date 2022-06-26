ed0a2939ae22aed4515f212e542295890a2e4e5f
Oleg Konan <o.konan@softteco.com>
2022-06-26 22:07:38 +0400

    added data

---
+++ src/data/projects.js
@@ -0,0 +1,127 @@
+export const projects = [
+  {
+    id: 1,
+    name: 'clock app',
+    description: 'A very simple app to show the clock with a second arrow',
+    route: 'clock-project',
+    link: 'https://olegkonan.github.io/simple-clock/',
+    picture:
+      'https://res.cloudinary.com/dyx6vimps/image/upload/v1656261562/projects/clock_iiic8z.jpg',
+    githubLink: 'https://github.com/olegkonan/simple-clock',
+  },
+  {
+    id: 2,
+    name: 'app for the theatre`s plot',
+    description:
+      'An app to display replicas of all characters of the theatre play with the possibility to choose concrete character and swich between replicas',
+    route: 'plot-project',
+    link: 'https://olegkonan.github.io/serenade-script/',
+    picture:
+      'https://res.cloudinary.com/dyx6vimps/image/upload/v1656261563/projects/plot_tvcrpx.jpg',
+    githubLink: 'https://github.com/olegkonan/serenade-script',
+  },
+  {
+    id: 3,
+    name: 'wild life landing',
+    description: 'A simple web page with html and css only',
+    route: 'wildlife-project',
+    link: 'https://rolling-scopes-school.github.io/olegkonan-JSFE2021Q1/wildlife/',
+    picture:
+      'https://res.cloudinary.com/dyx6vimps/image/upload/v1656261562/projects/wildlife_kamydx.jpg',
+    githubLink:
+      'https://github.com/rolling-scopes-school/olegkonan-JSFE2021Q1/tree/wildlife',
+  },
+  {
+    id: 4,
+    name: 'virtual piano app',
+    description:
+      'An app that can play piano sounds by clicking the keys or typing keyboard',
+    route: 'piano-project',
+    link: 'https://rolling-scopes-school.github.io/olegkonan-JSFE2021Q1/virtual-piano/',
+    picture:
+      'https://res.cloudinary.com/dyx6vimps/image/upload/v1656261562/projects/piano_uh4awt.jpg',
+    githubLink:
+      'https://github.com/rolling-scopes-school/olegkonan-JSFE2021Q1/tree/virtual-piano',
+  },
+  {
+    id: 5,
+    name: 'online zoo landing',
+    description: 'A simple web page with routes made by a figma design',
+    route: 'onlinezoo-project',
+    link: 'https://rolling-scopes-school.github.io/olegkonan-JSFE2021Q1/online-zoo/pages/main/',
+    picture:
+      'https://res.cloudinary.com/dyx6vimps/image/upload/v1656261562/projects/online-zoo_f1giny.jpg',
+    githubLink:
+      'https://github.com/rolling-scopes-school/olegkonan-JSFE2021Q1/tree/online-zoo',
+  },
+  {
+    id: 6,
+    name: 'photo filter app',
+    description:
+      'An app to upload picture, modify it with different filters and download the result',
+    route: 'photofilter-project',
+    link: 'https://rolling-scopes-school.github.io/olegkonan-JSFE2021Q1/photo-filter/',
+    picture:
+      'https://res.cloudinary.com/dyx6vimps/image/upload/v1656261562/projects/photo-filter_arb2wv.jpg',
+    githubLink:
+      'https://github.com/rolling-scopes-school/olegkonan-JSFE2021Q1/tree/photo-filter',
+  },
+  {
+    id: 7,
+    name: 'match match game',
+    description:
+      'A game with instructions, statistics and settings, the main aim of the game is to find matches in pictures as fast as it`s possible',
+    route: 'matchmatch-project',
+    link: 'https://rolling-scopes-school.github.io/olegkonan-JSFE2021Q1/match-match-game/dist/',
+    picture:
+      'https://res.cloudinary.com/dyx6vimps/image/upload/v1656261562/projects/match-match_qkej12.jpg',
+    githubLink:
+      'https://github.com/rolling-scopes-school/olegkonan-JSFE2021Q1/tree/match-match-game',
+  },
+  {
+    id: 8,
+    name: 'async race game',
+    description:
+      'This is the race, where you can create cars, and start a race with all cars or with selected only. The main idea is to use javascript Promises. This app requres an installation of the backend part',
+    route: 'asyncrace-project',
+    link: 'https://async-race.netlify.app/',
+    picture:
+      'https://res.cloudinary.com/dyx6vimps/image/upload/v1656261562/projects/async-race_owdyyg.jpg',
+    githubLink:
+      'https://github.com/rolling-scopes-school/olegkonan-JSFE2021Q1/tree/async-race',
+  },
+  {
+    id: 9,
+    name: 'english for kids game',
+    description:
+      'The game for learning english words by categories, It consists of 2 parts: learning and game. You can also see your statistics and practice the most difficult words',
+    route: 'englishforkids-project',
+    link: 'https://olegkonan-english-for-kids.netlify.app/',
+    picture:
+      'https://res.cloudinary.com/dyx6vimps/image/upload/v1656261562/projects/english-for-kids_zvzswk.jpg',
+    githubLink:
+      'https://github.com/rolling-scopes-school/olegkonan-JSFE2021Q1/tree/english-for-kids',
+  },
+  {
+    id: 10,
+    name: 'shop cart with stripe',
+    description:
+      'A simple online shop with goods fetched from the stripe service, it uses the stripe checkout component',
+    route: 'stripe-project',
+    link: 'https://test-stripe-basic.netlify.app/',
+    picture:
+      'https://res.cloudinary.com/dyx6vimps/image/upload/v1656261562/projects/stripe_okce2c.jpg',
+    githubLink: 'https://github.com/olegkonan/test-stripe-basic-client',
+  },
+  {
+    id: 11,
+    name: 'wish app',
+    description:
+      'An app for writing you wishes, creating categories for them and then playing a special psycological game. All data is stored in the browser in indexed db',
+    route: 'wish-project',
+    link: 'https://wish-app.netlify.app/',
+    picture:
+      'https://res.cloudinary.com/dyx6vimps/image/upload/v1656261562/projects/wish-app_jlwj7n.jpg',
+    githubLink: 'https://github.com/olegkonan/wish-app',
+  },
+];
