afc6a010b4f79c5ff891c51ea80906977995a124
Oleg Konan <o.konan@softteco.com>
2022-06-26 22:08:43 +0400

    connected react router to the app

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,20 @@
+import { Layout } from 'layout';
 import { MainPage } from 'pages';
+import { ProjectDetails } from 'pages/main/widgets/ProjectDetails';
+import { Routes, Route } from 'react-router-dom';
+import { routeParams, routes } from 'routes';
 
 export const App = () => {
-  return <MainPage />;
+  return (
+    <Routes>
+      <Route path={routes.root} element={<Layout />}>
+        <Route path={routes.root} element={<MainPage />}>
+          <Route
+            path={`:${routeParams.project}`}
+            element={<ProjectDetails />}
+          />
+        </Route>
+      </Route>
+    </Routes>
+  );
 };
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,9 +1,23 @@
-import { Layout } from 'layout';
+import styled from 'styled-components';
+import { Outlet } from 'react-router-dom';
+import { Navigation } from './widgets/Navigation';
+
+const PageContainer = styled.div`
+  display: flex;
+  gap: 2rem;
+`;
+
+const Content = styled.div`
+  flex: 1;
+`;
 
 export const MainPage = () => {
   return (
-    <Layout>
-      <p>Main Page</p>
-    </Layout>
+    <PageContainer>
+      <Navigation />
+      <Content>
+        <Outlet />
+      </Content>
+    </PageContainer>
   );
 };
